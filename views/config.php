2340616a0a97e8f5c2490b972b82e0362c7740f9
unknown <wilku@wilku-PC.(none)>
2012-03-23 21:34:35 +0100

    Signed-off-by: unknown <wilku@wilku-PC.(none)>

---
+++ views/config.php
@@ -0,0 +1,8 @@
+<?php defined('SYSPATH') or die('No direct script access.'); ?>
+
+<script type="text/javascript">
+        var RecaptchaOptions = {
+                lang : '<?php echo $lang ?>',
+                theme : '<?php echo $theme ?>',
+        };
+</script>
